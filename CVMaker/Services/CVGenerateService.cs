145b2ba25350491d3c4b506b1b9230bb41264f62
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-25 17:54:12 +0600

    cv generate service added

---
+++ CVMaker/Services/CVGenerateService.cs
@@ -0,0 +1,45 @@
+﻿using DinkToPdf;
+using DinkToPdf.Contracts;
+
+namespace CVMaker.Services
+{
+    public class CVGenerateService
+    {
+        private readonly IConverter _converter;
+
+        public CVGenerateService(IConverter converter)
+        {
+            this._converter = converter;
+        }
+
+        public byte[] Generate(string htmlContent)
+        {
+            var globalSettings = new GlobalSettings
+            {
+                ColorMode = ColorMode.Color,
+                Orientation = Orientation.Portrait,
+                PaperSize = PaperKind.A4,
+                Margins = new MarginSettings { Top = 10, Bottom = 10, Left = 10, Right = 10 },
+                DocumentTitle = "Genereated CV"
+            };
+
+            var objectSettings = new ObjectSettings
+            {
+                PagesCount = true,
+                HtmlContent = htmlContent,
+                //Page = "https://www.google.com/",
+                WebSettings = { DefaultEncoding = "utf-8" },
+                HeaderSettings = { FontSize = 12, Right = "Page [page] of [toPage]", Line = true, Spacing = 2.812 },
+                FooterSettings = { FontSize = 12, Line = true, Right = "© " + DateTime.Now.Year }
+            };
+
+            var document = new HtmlToPdfDocument()
+            {
+                GlobalSettings = globalSettings,
+                Objects = { objectSettings }
+            };
+
+            return _converter.Convert(document);
+        }
+    }
+}
